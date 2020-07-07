9c8a7b77db0e37dbf08e6f08321e3a85b1a53811
Cantando <65988365+Cantando@users.noreply.github.com>
2020-07-06 23:17:04 -0400

    added my github image

--- READEME.md
+++ READEME.md
@@ -1,8 +1,20 @@
-# l
-  jjjjh
-  l
-  wwwww
-  by mail
+
   https://github.com/Cantando
+  ![My Github Profile Pic](https://avatars1.githubusercontent.com/u/65988365?v=4)
+  # cool
+
+
+  ##Description
+  very cool
+
+  ##Contact me
+  by mail
+  myrnacantando@gmail.com
+  
+  
+  ## live demo
+  www.verycoolproject
+
+  ##Technologies
   ES6
 
--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,11 +1,23 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.project}
-  ${data.email}
-  ${data.description}
-  ${data.url}
-  ${data.contact}
+  return `
   ${data.html_url}
+  ![My Github Profile Pic](${data.avatar})
+  # ${data.project}
+
+
+  ##Description
+  ${data.description}
+
+  ##Contact me
+  ${data.contact}
+  ${data.email}
+  
+  
+  ## live demo
+  ${data.url}
+
+  ##Technologies
   ${data.technology}
 
 `;
